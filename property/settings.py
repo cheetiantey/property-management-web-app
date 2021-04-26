9cea1b949d3b5b98edeb3d890b589db0c6acfc3a
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-04-25 22:34:20 -0500

    Fix database issues

--- property/settings.py
+++ property/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'property.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3'',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'USER': '',
         'PASSWORD': '',
